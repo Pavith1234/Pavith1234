fc5aa4293205880b31ba58a9d34f45946379d774
Pavithra Madipeddi <103506288+Pavith1234@users.noreply.github.com>
2025-03-26 22:17:19 +0530

    Add files via upload
    This is a Program in scala how to read the inputs using different data types

---
+++ User.scala
@@ -0,0 +1,14 @@
+object User
+{
+ def main(args:Array[String]):Unit=
+ {
+  var x:Int=0;
+   x= scala.io.StdIn.readLine().toInt;
+   var y:String="";
+  print("Value of x:");
+  println(x);
+y= scala.io.StdIn.readLine();
+print("Value of y:");
+print(y);
+}
+} 
