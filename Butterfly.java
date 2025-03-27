2d8a295dcc9c781ec47f289b37da9a8ef5be0800
Pavithra Madipeddi <103506288+Pavith1234@users.noreply.github.com>
2025-03-27 23:00:38 +0530

    Add files via upload

---
+++ Butterfly.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+class Butterfly
+{
+public static void main(String args[])
+{
+ Scanner sc = new Scanner(System.in);
+ int n=sc.nextInt();
+ for(int i=1;i<=n;i++)
+ {
+  char ch= 'A';
+  for(int j=1;j<=i;j++)
+  {
+   System.out.print(ch+" ");
+   ch++;
+  }
+  for(int j=1;j<=n-i;j++)
+  {
+   System.out.print("  ");
+   }
+   ch--;
+   for(int j=1;j<=i;j++)
+   {
+    System.out.print(ch +" ");
+     ch--;
+   }
+  System.out.println();
+  }
+   for(int i=1;i<n;i++)
+ {
+  char ch= 'a';
+  for(int j=1;j<=n-i;j++)
+  {
+   System.out.print(ch+" ");
+   ch++;
+  }
+  for(int j=1;j<=i;j++)
+  {
+   System.out.print("  ");
+   }
+   ch--;
+   for(int j=1;j<=n-i;j++)
+   {
+    System.out.print(ch +" ");
+     ch--;
+   }
+  System.out.println();
+  }
+
+}
+}
